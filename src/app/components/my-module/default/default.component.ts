8b11c3b12f671b79607215174724b362ce766d91
RyamSousa <ryam.sousa123@gmail.com>
2022-07-13 11:43:20 -0300

    :sparkles: added dynamic creation feature

--- src/app/components/my-module/default/default.component.ts
+++ src/app/components/my-module/default/default.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import {
+	Component,
+	ComponentFactory,
+	ComponentFactoryResolver,
+	ComponentRef,
+	OnInit,
+	ViewChild,
+	ViewContainerRef,
+} from "@angular/core";
+import { DynamicComponent } from "../dynamic/dynamic.component";
 
 @Component({
 	selector: "app-default",
@@ -6,7 +15,20 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./default.component.scss"],
 })
 export class DefaultComponent implements OnInit {
-	constructor() {}
+	@ViewChild("dynamicComponent", { read: ViewContainerRef }) container;
+
+	componentType: ComponentFactory<DynamicComponent>;
+	componentRef: ComponentRef<DynamicComponent>;
+
+	constructor(private resolver: ComponentFactoryResolver) {}
 
 	ngOnInit(): void {}
+
+	create() {
+		this.componentType = this.resolver.resolveComponentFactory(DynamicComponent);
+		console.log(this.componentType);
+		this.componentRef = this.container.createComponent(this.componentType);
+		console.log(this.componentRef);
+		this.componentRef.instance;
+	}
 }
